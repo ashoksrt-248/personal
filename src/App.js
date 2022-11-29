737c85622f6dc3957f6729754a8fba9f5eca5c18
Ashok Amara <ashoka@infinite.com>
2022-11-29 12:03:23 +0530

    scrolling changes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
         <h1>React personal portfolio</h1>
         <marquee  direction="left"><h2>Ashok Amara</h2></marquee>
         <marquee direction="right"><h2>personal Portfolio</h2></marquee>
-        <marquee direction="up"><h2>Personal Website</h2></marquee>
-        <marquee direction="down"><h2>React Project</h2></marquee>
+       
       </header>
     </div>
   );
